2257003f04aa6d829651737f691d5a617133f0d7
shubhada yadav <shubhada.yadav107@gmail.com>
2025-12-15 19:54:10 +0530

    Update

--- If_else.js
+++ If_else.js
@@ -1,11 +1,11 @@
-let age = 40;
-let citizen = "Korea";
+let age = 10;
+let citizen = true;
 let registeredToVote = true;
 
-if (age >= 18 && citizen == "India" && registeredToVote == true) {
+if (age >= 18 && citizen == true && registeredToVote == true) {
   console.log("you are eligible to vote");
   //18 year or older , a citizen , registerd to vote
-} else if (age >= 18 && citizen !== "India") {
+} else if (age >= 18 && citizen !== true) {
   console.log("not able due to citizenship status");
   // 18 year or older, but not citizenship.
 } else if (age >= 18 && registeredToVote !== true) {
